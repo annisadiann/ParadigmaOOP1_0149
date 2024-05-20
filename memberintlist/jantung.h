3353361d2f14da6fcf246b727948c25d695f3b11
annisadiann <annisadian0412@gmail.com>
2024-05-20 10:56:39 +0700

    menambahkan kelas jantung dengan konstruktor

---
+++ memberintlist/jantung.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+#include <iostream>
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	
+};
+#endif
