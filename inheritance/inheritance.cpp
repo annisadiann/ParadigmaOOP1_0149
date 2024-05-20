66c637bf471bdae2b54752c2b323c5796e9831cf
annisadiann <annisadian0412@gmail.com>
2024-05-20 18:24:03 +0700

    menambahkan konstruktor dan destruktor pada kelas orang

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -6,5 +6,11 @@ class orang {
 public:
 	string nama;
 
-
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
 };
