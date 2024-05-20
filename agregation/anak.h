f668c99b6dd3532a2d1d4e7d47816864633669e4
annisadiann <annisadian0412@gmail.com>
2024-05-20 11:49:51 +0700

    pembetulan

---
+++ agregation/anak.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef ANAK_H
+#define ANAK_H
+using namespace std;
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
