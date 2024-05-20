45347a5b97498c2ea1d6c0053c5cedc9955f8e9d
annisadiann <annisadian0412@gmail.com>
2024-05-20 10:25:27 +0700

    Add project files.

---
+++ memberintlist/memberintlist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
